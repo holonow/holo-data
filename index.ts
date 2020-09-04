b198b5f171bfb39745bc251171769a693eb877cb
wabilin <twelendil@protonmail.com>
2020-09-04 20:52:37 +0900

    build for workflow

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import parseScheduleHtml from 'holo-schedule';
 import getScheduleHtml from 'holo-schedule/lib/getScheduleHtml';
 import { writeFile } from 'fs';
-import axios from 'axios'
+// import axios from 'axios'
 
-const MAP_URL = 'https://holonow.github.io/data/imageMap.json'
+// const MAP_URL = 'https://holonow.github.io/data/imageMap.json'
 
 function write(path: string, data: string): Promise<void> {
   return new Promise((resolve, reject) => {
@@ -18,14 +18,15 @@ function write(path: string, data: string): Promise<void> {
 }
 
 async function main() {
-  const { data } = await axios.get(MAP_URL)
+  // const { data } = await axios.get(MAP_URL)
+  const data = {}
 
   const html = await getScheduleHtml();
   const { lives, dict } = parseScheduleHtml(html, data);
 
   return Promise.all([
-    write('public/data/schedule.json', JSON.stringify(lives)),
-    write('public/data/imageMap.json', JSON.stringify(dict)),
+    write('build/schedule.json', JSON.stringify(lives)),
+    write('build/imageMap.json', JSON.stringify(dict)),
   ]);
 }
 
